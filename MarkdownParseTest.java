6c78ff85aaafe719977230b29a7b225b1bcc9d26
Kailey Wong <kaileyswong@gmail.com>
2022-04-21 18:48:16 -0700

    test-file.md passes test

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,15 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.List;
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.io.IOException;
 
 public class MarkdownParseTest {
     @Test
-    public void addition() {
-    assertEquals(2, 1 + 1);
+    public void testfileCorrectLinks() throws IOException {
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        assertEquals(List.of("https://something.com", "some-thing.html"), MarkdownParse.getLinks(content));
     }
 }
